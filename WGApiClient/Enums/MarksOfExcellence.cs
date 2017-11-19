4b21774f047525820a659d43ab1d53ae4a12cd49
Aimdrol <accounts_aimdrol@hotmail.com>
2017-11-19 03:40:16 +0100

    Added MarksOfExcellence enum

---
+++ WGApiClient/Enums/MarksOfExcellence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WGApi.Enums
+{
+    [Flags]
+    public enum MarksOfExcellence
+    {
+        None = 0,
+        One = 1 << 0,
+        Two = 1 << 1,
+        Three = 1 << 2,
+
+        All = ~(~0 << 3),
+    }
+}
